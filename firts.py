7ca1e92574a0721c06a3e7b9052f7c0a136cfdb4
Nirbhay kakadiya <nirbhaykakadiya11@gmail.com>
2024-09-09 15:24:19 +0530

    Add files via upload

---
+++ firts.py
@@ -0,0 +1,283 @@
+# print('''Name:- Kakadiya Nirbhay
+# Birth of date:- 10/9/2005
+# std:-B.com
+# city:-jasdan
+# distac:-rajkot
+# state:-gujrat 
+#       ''')
+
+# print("ghnf")
+
+# comments
+
+# singale line comments
+# oihikfnjdboi
+
+# Multi line comments
+# '''vjijipjipdejp'''
+
+# my name sagar dholariya
+
+# a=10
+# b=15
+# c=12
+# if a>b :
+#     print("a is max")
+# elif b>c:
+#     print("b is max") 
+# elif c>a:
+#     print("c is max")
+# else:
+#     print("anviled")
+
+
+# a=input("enter the value:-")
+# b=input("enter the value:-")
+# c=input("enter the value:-")
+# if a>b:
+#     print("a is max")
+# elif b>c:
+#     print("b is max")
+# elif c>a:
+#     print("c is max") 
+# else:
+#     print("anviled")          
+
+# sohamAmiparaTransGlobe = ()  # Camel Case
+# SohamAmiparaTransGlobe = ()  # Pascal Case
+# soham_amipara_trans_globe = ()  # Snake Case 
+
+# maths_marks = int(input("Enter Maths Marks: "))
+# science_marks = int(input("Enter Science Marks: "))
+# physics_marks = int(input("Enter Physics Marks: "))
+
+# if maths_marks > science_marks:
+#     if maths_marks > physics_marks:
+#         print(f"Your Maths Marks is {maths_marks}. So you are good in Maths.")
+#     else:
+#         print(f"Your Physics Marks is {physics_marks}. So you are good in Physics.")
+# elif science_marks > physics_marks:
+#     print(f"Your science marks is {science_marks}. so you are good in science.")
+# else:
+#     print(f"Your Physics Marks is {physics_marks}. So you are good in Physics.")      
+
+# num=int(input("enter the value:-"))
+# if num % 2 == 0:
+#     print("even number")
+# else:
+#     print("odd number")  
+
+# l=int(input("enter the value:-"))
+# b=int(input("enter the value:-"))
+# if l==b:
+#     area = l*l
+#     print("area of square",area)
+# elif l<b:
+#     area=l*b
+#     print("area of rectangle:",area)
+# else:
+#     print("error")       
+
+# a=int(input("enter first number:-"))
+# b=int(input("enter second number:-"))
+
+# if a<b:
+#     print("smaller",a)
+#     print("larger",b)
+# else:
+#     print("smaller",b)
+#     print("larger",a)
+
+# a=int(input("enter first numbre:-"))
+# b=int(input("enter second numbre:-"))
+# c=int(input("enter third numbre:-"))
+
+# if a>b:
+#     print("number of larger1",a)
+# elif b>c:
+#     print("number of larger2",b)
+# else:
+#     print("number of larger3",c)        
+
+# a=int(input("enter first numbre:-"))
+# b=int(input("enter second numbre:-"))
+# c=int(input("enter third numbre:-"))
+
+# if a<b:
+#     print("number of smaller1",a)
+# elif b<c:
+#     print("number of smaller2",b)
+# else:
+#     print("number of smaller3",c)        
+
+# a  =20
+# b = 30
+# c = 40
+# if a==10:
+#     print("Right numbers of A")
+# elif b != 30:
+#     print("Right numbers of B")
+# elif c==40:
+#     print("Right numbers of C")
+# else:
+#     print("Enter a right numbers")
+
+# d = 20
+# e = 23
+# f = 34
+
+# if d>=e:
+#     print("Right")
+# elif e>=f:
+#     print("Right Now")
+# elif f>=d:
+#     print("right nows")
+# elif d<=e:
+#     print("hello bro")
+# elif e<=f:
+#     print("hii brother")
+# elif f<=d:
+#     print("Hello Nirbhay")
+# else:
+#     print("you are a wrong")
+
+# True variables
+
+# x = 10
+# X = 20
+# x_12 = 23
+# _y_y = 23
+# s2 = 24
+# u__6  = 345
+
+# Flase variables
+
+# @f  = 23
+# %dg = 46
+# 1d =43
+
+# data types
+
+# int 
+# str
+# float
+# list
+# tuples
+# dict
+# sets
+# complex
+
+# x = 10
+# print(x)
+
+# x = 10
+# y =20
+# z  =30
+# print(x+y+z)
+
+# a=input("enter first numbre:-")
+# b=input("enter second numbre:-")
+# p=input("enter choise:- ")
+# if p == "+":
+#     print(a+b)
+# elif p == "-":
+#     print(a-b)
+# elif p == "*":
+#     print(a*b)
+# elif p == "/":
+#     print(a/b)
+# else:
+#     print("enter your choise")
+
+# if 5!=5:
+#     print("Right")
+# else:
+#     print("Wrong")
+
+# String
+
+# x = "hello"
+# print(x)
+# print(type(x))
+
+# y =str("demo")
+# print(y)
+
+# z = 'nirbhay'
+# print(z)
+
+# Float
+
+# x = '1.2'
+# print(x)
+# print(type(x))
+
+# x = int('1.2')
+# print(x)
+
+# x = "my name is nirbhay"
+# print(x[7:2:-1])
+
+
+# x = "my name is nirbhay"
+# print(x[:2]+x[7:2:-1]+x[7:])
+
+# x = [1,"hello",1.2,3,4,5,6,7,8,9]
+
+# x.append('nirbhay')
+# print(x)
+# x.clear()
+# print(x)
+# x.copy()
+# print(x)
+
+# fruits = ['banana','apple', 'cherry']
+
+# x = fruits.count('cherry')
+# print(x)
+
+# x=fruits.index("apple")
+# print(x)
+
+# fruits.insert(1, "orange")
+# print(fruits)
+
+# fruits.remove("apple")
+# print(fruits)
+
+# x = fruits.pop(2)
+# print(x)
+
+# fruits.reverse()
+# print(fruits)
+
+# fruits.sort()
+# print(fruits)
+
+# fruits = ['apple', 'banana', 'cherry']
+# cars = ['Ford', 'BMW', 'Volvo']
+
+# fruits.extend(cars)
+# print(fruits)
+
+# my name is nirbhay my class name is transglobe multimedia
+
+# x="my name is nirbhay my class name is transglobe multimedia"
+# print(x[:2]+x[7:2:-1]+x[7:10]+x[18:10:-1]+x[18:21]+x[27:21:-1]+x[27:35]+x[46:35:-1]+x[46:])
+
+# print(x[:10]+x[18:10:-1]+x[18:47]+x[57:45:-1])
+
+# tuples
+
+# "anviled"
+
+# x= (1,2,3,4,"hello","demo",2.23,6,7,8,90)
+# print(x)
+# print(x[7:2:-1])
+
+
+# email validation
+email=input("enter your email:-")
+if :
+    
+    
