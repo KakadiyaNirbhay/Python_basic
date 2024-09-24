07ee95271399b60807eab3f7fab80e471ec6d0a1
Nirbhay kakadiya <nirbhaykakadiya11@gmail.com>
2024-09-24 14:41:14 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,115 @@
+# import qrcode
+# x=qrcode.make("https://github.com/KakadiyaNirbhay/python")
+# x.save('nk.png')
+
+# import qrcode
+# x=qrcode.make("nirbhay kakadiya")
+# x.save('nk1.png')
+
+# for i in range(5):
+#     i = int(input("enter a number 1 to 100:- "))
+#     if 1 <= i <= 100 :
+#         print ("velid number")
+#         break
+
+
+# adj=["red","big","tasty"]
+# fruits=["apple","banana","cherry"]
+# for x in adj:
+#     for y in fruits:
+#         print(x,y)
+
+
+# i=2
+# while i<=10:
+#     print(i)
+#     i+=2
+    
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=2
+    
+# i=2
+# while i<=10:
+#     if i%2==0:
+#         print(i)
+#     i+=2    
+    
+# for i in range(1,10,2):
+#     print(i)
+
+# for i in range(2,11,2):
+#     print(i)
+
+# x= input("enter the value:-")
+# i=0
+# while i<len(x):
+#     if x[i]== " ":
+#         break
+#     print(x[i],end="")
+#     i+=1
+
+# for i in range (len(x)):
+#     if x[i]== " ":
+#         break
+#     print(x[i],end="")
+
+
+# i=1
+# while i <= 4:
+#     print(i)
+    # i+=1
+    
+# for i in range(1,5):
+#     print(i)
+
+# for i in "banana":
+#     print(i)
+
+# i=0
+# while i < len("banana"): 
+#     print("banana"[i])
+#     i+=1
+
+# for i in range(1,6):
+#     print("*"*i)
+
+# i=1
+# while i<=10:
+#     if i%2==0:
+#         pass
+#     else:
+#         print(i*1,i*2,i*3,i*4,i*5)
+#     i+=1   
+
+# for i in range(1,6):
+#     print(("*")*i)
+
+# for i in range(1,6+1):
+#     for x in range(1,i,1):
+#         print(x,end="")
+#     print("")
+
+# for i in range(6):
+#     for x in range(i):
+#         print(i,end="")    
+#     print("")
+
+# for i in range(5):
+#     char = chr(65 + i)
+#     print(char * (i+1))
+    
+# for i in range(5,0,-1):
+#     print(("*")*i)
+
+# for i in range(5,0,-1):
+#     for x in range(i):
+#         print(i ,end="")
+#     print("")    
+
+# for i in range(5,0,-1):
+#     for x in range(i,0,-1):
+#         print(x,end='')
+#     print("")
---
+++ 2.py
@@ -0,0 +1,44 @@
+#q-1
+# gudio van russam
+# in 1991
+
+# q-2
+# import sys 
+# print(sys.version)
+
+# q-3
+# x={"name":"nirbhay","age":20,"number":8388673,"email":"nirbhay@gmail","address":"lilapur"}
+# print (dict.keys(x))
+# q-5
+# i=1
+# while i <= 10:
+#     print(i*1,i*2,i*3,i*4,i*5)
+#     i+=1
+
+# q-9
+# set
+# x={1,2,435,3,4}
+# y={'a','s','d','v'}
+# print(x)
+# print(y)
+# print(type(x))
+# z=x.union(y)
+# print(z)
+# x.difference_update(y)
+# print(x)
+# z=x.issuperset(y)
+# print(z)
+# z=x.issubset(y)
+# print(z)
+# x.discard(435)
+# print(x)
+# z=x.symmetric_difference(y)
+# print(z)
+# z=x.isdisjoint(y)
+# print(z)
+# z=x.intersection(y)
+# print(z)
+# z=x.difference(y)
+# print(z)
+# x.intersection_update(y)
+# print(x)
