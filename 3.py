4e7e635176bf526f5701b7997b0d5cb243e33e35
Nirbhay kakadiya <nirbhaykakadiya11@gmail.com>
2024-10-01 10:22:24 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,58 @@
+# x= "my name is Nirbhay"
+# print("is" in x)
+
+# x= "My name is nk"
+
+
+# if "nk"in x:
+#     print("nk right")
+# else:
+#     print("not right")
+
+# if "hello" not in x:
+#     print("right")
+# else:
+#     print("hello is not right")
+
+
+# if "My" in x:
+#     print("my is velid")
+# elif "demo" not in x:
+#     print('demo is true')
+# else:
+#     print("invelid")    
+
+
+
+# y= "Kakadiya Nirbhay"
+# print(y.upper())
+# print(y.lower())
+# print(y.strip())
+# print(y.find("a"))
+# print(y.replace("k","y"))
+
+
+# a="Nirbhay"
+# b=30
+# print(f"my name is {a} my age is {b}")
+
+# a=(f" my age is {20+30}")
+# print(type(a))
+# print(a)
+
+
+# x=74
+# y=84
+# print(F"MY AGE IS {x+y}")
+# print(F"MY AGE IS {x-y}")
+# print(F"MY AGE IS {x*y}")
+# print(F"MY AGE IS {x/y}")
+# print(F"MY AGE IS {x,y}")
+
+
+# NIRBHAY="nirbhya"
+# print(F'HII,{NIRBHAY.capitalize()}')
+
+
+# x= 4.7982379
+# print(f"{x:.1f}")
